23f855a96b98f2375812a4c1432c9df8b40c8134
Roman Gruba <roman.gruba@gmail.com>
2019-09-02 00:17:53 +0300

    proptypes left

---
+++ build/precache-manifest.3e7dde72dc38fd6ed0b9e13d42169968.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "031755609a07e52cdc18f398d07efff2",
+    "url": "/goit-react-hw-02-movies/index.html"
+  },
+  {
+    "revision": "7bffc7f7be3721652fc6",
+    "url": "/goit-react-hw-02-movies/static/css/main.d280dd98.chunk.css"
+  },
+  {
+    "revision": "40df87cf9fa3037d6c96",
+    "url": "/goit-react-hw-02-movies/static/js/2.0e69e4bd.chunk.js"
+  },
+  {
+    "revision": "7bffc7f7be3721652fc6",
+    "url": "/goit-react-hw-02-movies/static/js/main.73ba7831.chunk.js"
+  },
+  {
+    "revision": "11c478ff6fe57073f42a",
+    "url": "/goit-react-hw-02-movies/static/js/runtime~main.fcb629fb.js"
+  }
+]);
